7cd7ad12e3736d62dccf19dab9c60919007dd608
Efim Kubishkin <efimkub@mail.ru>
2024-07-24 18:12:37 +0300

    Add pairwise and sum_reduce examples

---
+++ src/SumReduce.hs
@@ -0,0 +1,34 @@
+module SumReduce where
+
+import Clash.Prelude
+
+sumReduce
+  :: Int
+  -> Int
+  -> (Int, Int)
+sumReduce acc num = (mySum, mySum)
+  where
+    mySum = acc + num
+
+mealySumReduce
+  :: (KnownDomain dom, HiddenClockResetEnable dom)
+  => Signal dom Int
+  -> Signal dom Int
+mealySumReduce = mealy sumReduce 0
+
+{-# ANN
+  topEntity
+  ( Synthesize
+      { t_name = "sumReduce"
+      , t_inputs = [PortName "CLK", PortName "RST", PortName "ENBL", PortName "NUM"]
+      , t_output = PortName "SUM"
+      }
+  )
+  #-}
+topEntity
+  :: Clock System
+  -> Reset System
+  -> Enable System
+  -> Signal System Int
+  -> Signal System Int
+topEntity = exposeClockResetEnable mealySumReduce
